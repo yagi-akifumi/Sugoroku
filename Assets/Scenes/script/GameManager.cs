e93d1ab3e6bf59edea7c01bc23b32f5302506c67
八木暁史 <>
2026-03-20 23:04:47 +0900

    ぴょんぴょん動く

--- Assets/Scenes/script/GameManager.cs
+++ Assets/Scenes/script/GameManager.cs
@@ -1,21 +1,37 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour
 {
-
     public PlayerManager player;
     public List<Transform> tiles;
 
-    //public void MoveOneStep()
-    //{
-    //player.currentIndex = (player.currentIndex + 1) % tiles.Count;
-    //player.transform.position = tiles[player.currentIndex].position;
-    //}
+    private bool isMoving = false;
 
     public void MoveSteps(int step)
     {
-        player.currentIndex = (player.currentIndex + step) % tiles.Count;
-        player.transform.position = tiles[player.currentIndex].position;
+        if (isMoving) return;
+        StartCoroutine(MoveCoroutine(step));
+    }
+
+    IEnumerator MoveCoroutine(int step)
+    {
+        isMoving = true;
+
+        for (int i = 0; i < step; i++)
+        {
+            player.currentIndex = (player.currentIndex + 1) % tiles.Count;
+
+            Vector3 targetPos = tiles[player.currentIndex].position;
+
+            yield return player.transform
+                .DOJump(targetPos, 0.5f, 1, 0.3f)
+                .SetEase(Ease.OutQuad)
+                .WaitForCompletion();
+        }
+
+        isMoving = false;
     }
 }
